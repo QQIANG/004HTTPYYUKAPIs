80ecaf841d62ac255770ab0f9ad130ee6e4ad43f
JNYJ(William.zhou) <qqiang.dev@gmail.com>
2015-01-31 08:08:39 +0800

    ADD JNYJ SWIFT FILE

---
+++ 004HTTPYYUKAPIs(system)/004HTTPYYUKAPIs(system)/JNYJ.swift
@@ -0,0 +1,16 @@
+//
+//  JNYJ.swift
+//  004HTTPYYUKAPIs(system)
+//
+//  Created by JNYJ on 14-11-18.
+//  Copyright (c) 2014年 JNYJ. All rights reserved.
+//
+
+//AFNetworking upload images
+//http://stackoverflow.com/questions/19261481/ios-image-upload-via-afnetworking-2-0
+
+import UIKit
+
+class JNYJ: NSObject {
+   
+}
